78033c53e83425cac81911b76224716f4752670d
Penny Train <penny.train04@gmail.com>
2024-04-28 15:14:39 +0100

    Edit Accounts model to fix bug

--- accounts/models.py
+++ accounts/models.py
@@ -1,29 +1,16 @@
 from django.db import models
-from django.db.models.signals import post_save
+from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
+
 
 # Create your models here.
 
 class Accounts(models.Model):
-    owner = models.OneToOneField(User, on_delete=models.CASCADE),
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    username = models.CharField(max_length=150, blank=True) 
-    body = models.TextField(blank=True)
-    image = models.ImageField(
-        upload_to='images/', default="../profile_kqgeo6.jpg"
-    )
-
-    # CHANGE THE DEFAULT PROFILE IMAGE
-
-    class Meta: 
-        ordering = ["-created"] 
+    """
+    Main Model for the users profile
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_image = CloudinaryField('image', default="https://res.cloudinary.com/dgz5gpe5z/image/upload/v1708453621/owy10e5dfvswqrozcc1m.png")
 
     def __str__(self):
-        return f"{self.owner}'s account"
-
-def create_account(sender, instance, created, **kwargs):
-    if created:
-        Accounts.objects.create(owner=instance)
-
-post_save.connect(create_account, sender=User)
+        return f'{self.user.username}'
